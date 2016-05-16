f6d790ee39c08bf9ba85863c606bc308ab0270f6
Dylan Chong <dylan.chong@icloud.com>
2016-05-17 09:48:33 +1200

    Fixed git ignore and added main classes

---
+++ src/DiagramEditor.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Dylan on 17/05/16.
+ */
+public class DiagramEditor {
+
+}
---
+++ src/DiagramEditorMain.java
@@ -0,0 +1,12 @@
+import ecs100.UI;
+
+/**
+ * Created by Dylan on 17/05/16.
+ */
+public class DiagramEditorMain {
+
+    public static void main(String[] args) {
+        UI.initialise();
+    }
+
+}
