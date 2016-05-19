ec06a69b9b50603eedb3b18cbc5d5d2d943da07e
Dylan Chong <dylan.chong@icloud.com>
2016-05-19 21:14:56 +1200

    Added new BoundsCalculator class with TODOs

---
+++ src/DEDraggableBoundsCalculator.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Dylan on 19/05/16.
+ */
+public class DEDraggableBoundsCalculator {
+
+    // TODO NEXT move the DEObject new position calculation code here
+    // TODO AFTER use this class in DENode
+
+}
