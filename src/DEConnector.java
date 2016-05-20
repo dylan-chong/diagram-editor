fa5d9df36d1ccc70faf789f4575128dc32288237
Dylan Chong <dylan.chong@icloud.com>
2016-05-20 15:42:27 +1200

    Implemented drawing of connectors

--- src/DEConnector.java
+++ src/DEConnector.java
@@ -1,3 +1,5 @@
+import ecs100.UI;
+
 import java.awt.*;
 
 /**
@@ -18,7 +20,10 @@ public class DEConnector {
     }
 
     public void draw() {
-        // TODO: 20/05/16
+        UI.setColor(color);
+        DEPoint pa = connectedObjectA.getBounds().getCenter();
+        DEPoint pb = connectedObjectB.getBounds().getCenter();
+        UI.drawLine(pa.getX(), pa.getY(), pb.getX(), pb.getY());
     }
 
     public boolean isConnectedToObject(DEObject obj) {
