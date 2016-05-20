2b136e16f47f26b655934add3df28dc79eff13a9
Dylan Chong <dylan.chong@icloud.com>
2016-05-20 14:26:15 +1200

    Implemented label and label drawing in Shape

--- src/DEObjectShape.java
+++ src/DEObjectShape.java
@@ -1,3 +1,5 @@
+import ecs100.UI;
+
 import java.awt.*;
 
 /**
@@ -9,9 +11,13 @@ public abstract class DEObjectShape extends DEObject {
 
     public static final Color DEFAULT_FILL_COLOR = null;
     public static final Color DEFAULT_EDGE_COLOR = Color.BLACK;
+    public static final double TEXT_DISTANCE_FROM_LEFT = 3.0;
+    public static final double TEXT_DISTANCE_FROM_TOP = TEXT_DISTANCE_FROM_LEFT;
 
     protected Color fillColor = DEFAULT_FILL_COLOR;
     protected Color edgeColor = DEFAULT_EDGE_COLOR;
+
+    private String labelText;
 
     public DEObjectShape(DEBounds bounds) {
         super(bounds);
@@ -20,12 +26,22 @@ public abstract class DEObjectShape extends DEObject {
     @Override
     public void draw() {
         super.draw();
-        // TODO draw text
+        if (labelText != null) drawLabelText();
     }
 
     @Override
     public String toString() {
         return getClass().getName() + ": " + getBounds().toString();
+    }
+
+    private void drawLabelText() {
+        DEBounds b = getBounds();
+        UI.drawString(labelText, b.getLeft() + TEXT_DISTANCE_FROM_LEFT,
+                b.getTop() + TEXT_DISTANCE_FROM_TOP);
+    }
+
+    public void setLabelText(String text) {
+        labelText = text;
     }
 
 }
